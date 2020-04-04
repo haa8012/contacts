1f5744c77f0674b20240e01bacacda9e402055da
Hani A <ha8012@yahoo.com>
2020-04-04 17:50:19 -0400

    Express server setup

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-const express = require("express");
-
+const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the contact Keeper API" })
+//Connect database
+connectDB();
+
+//Init Middlewere
+app.use(express.json({ extended: false }));
+
+app.get(
+  '/',
+  (req, res) => res.json({ msg: 'Welcome to the contact Keeper API' })
+
+  //since this is a json api we use res.json
+  //you can also use res.send('to send a string or HTML')
+  //you can also send a file to the browser using res.sendFile()
 );
 
 // Defin Routes
-app.use("/api/users", require("./routes/users"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/contacts", require("./routes/contacts"));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
